d9bae28e83f8350a7bf979c35519976e0c38aa3b
email <bt22cse107@iiitn.ac.in>
2023-11-09 14:36:36 +0530

    added files

---
+++ first.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "every age has a language of its own";
+    return 0;
+}
---
+++ minheap.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void swap(int x, int y){
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+void heapify(int x[], int n){
+    
+}
+
+
+
+
+
+int main()
+{
+    int heap[100];
+    int n, i;
+
+    scanf("%d",n);
+    for(i=0;i<n;i++)scanf("%d",&heap[n]);
+
+
+
+    return 0;
+}
